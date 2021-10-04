a33dbf9bb0503575db098e6bd471ccffa59dd69f
Polina Filippova <polina17012001@mail.ru>
2021-10-04 12:29:31 +0300

    version4

--- src/js/checkNumber.js
+++ src/js/checkNumber.js
@@ -1,4 +1,7 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable operator-linebreak */
+/* eslint-disable linebreak-style */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
 /* eslint-disable padded-blocks */
@@ -12,22 +15,21 @@ export default function checkNumber(n) {
   if (card.startsWith('4') && card.length >= 13 && card.length <= 19) {
     return 'Visa';
   }
-  if (((sixCount >= 222100 || sixCount <= 272099) || [51, 52, 53, 54, 55]
-      .some((e) => card.startsWith(e))) && (card.length === 16)) {
+  if (((sixCount >= 222100 && sixCount <= 272099) || [51, 52, 53, 54, 55].some((e) => card.startsWith(e))) && (card.length === 16)) {
     return 'MasteCard';
   }
-  if ((fourCount >= 3528 || fourCount <= 3589)
-    .some((e) => card.startsWith(e)) && card.length >= 16 && card.length <= 19) {
+  if ((fourCount >= 3528 && fourCount <= 3589) &&
+    (card.length >= 16 && card.length <= 19)) {
     return 'JCB';
   }
-  if (((sixCount >= 622126 || sixCount <= 622925) || [6011, 644, 645, 646, 647, 648, 649, 65]
-      .some((e) => card.startsWith(e))) && (card.length >= 16 && card.length <= 19)) {
+  if (((sixCount >= 622126 && sixCount <= 622925) || [6011, 644, 645, 646, 647, 648, 649, 65].some((e) => card.startsWith(e))) &&
+    (card.length >= 16 && card.length <= 19)) {
     return 'Discover';
   }
   if (card.startsWith('36') && card.length === 14) {
     return 'Diners Club International';
   }
-  if ([34, 37].some((e) => card.startsWith(e)) && card.length === 15) {
+  if ([34, 37].some((e) => card.startsWith(e)) && (card.length === 15)) {
     return 'American Express';
   }
   if (card.startsWith('2') && card.length === 16) {
